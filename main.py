759aca4d5b264c3ad1c92a7155079763912f551f
BonzaiBrains <ahoemann@proton.me>
2024-06-25 21:08:11 +0200

    fix: syntax

--- main.py
+++ main.py
@@ -2,54 +2,49 @@
 import json
 import logging
 import subprocess
+#import get_values
 from flask import Flask, request, Response
 
 # Static JSON file for configuration
 JSON_FILE = "example.json"
 # Logging initialization
 logger = logging.getLogger(__name__)
-# Flask initialization
+
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=65187)
 
-A class for handeling configuration data
-class plugin:
-	def get_value_from_json(JSON_FILE, key, sub_key):
-		try:
-			with open(JSON_FILE) as f:
-				data = json.load(f)
-				return data[key][sub_key]
-		except Exception as err:
-			print("ERROR: ", err)
+def get_values(key):
+        try:
+            with open(JSON_FILE) as f:
+                data = json.load(f)
+                return data[key]
+        except Exception as err:
+            print("ERROR: ", err)
 
-# Initialization of objects
-	def __init__(self):
-		self.shell_command = get_value_from_json("shell_command")
-		self.api_key = get_value_from_json("api_key")
-		self.path_to_logs = get_value_from_json("path_to_logs")
-
-# Running commands read by the plugin class
+# Running commands read by the get_values class
 def run_shell_command():
-	for sh_cmd in plugin.shell_command():
-		return_code = int(subprocess.call(plugin.shell_command(), shell=True))
-		if return_code == 0:
-			logger.info(sh_cmd + ": successful")
-			return 0
-		else:
-			logger.info(sh_cmd + ": failed")
-			return return_code
-			continue
-
-# Runs shell commands when webrequest is recieved
-def deploy():
-	if request.method == 'POST':
-		print("Data received from Webhook is: ", request.json)
-		run_shell_command()
-		return 
-
+    for sh_cmd in get_values("shell_command"):
+        return_code = int(subprocess.call(sh_cmd, shell=True))
+        if return_code == 0:
+            logger.info(sh_cmd + ": successful")
+            print(sh_cmd)
+            return return_code
+        else:
+            logger.info(sh_cmd + ": failed")
+            return return_code
+  
 def main():
-	logging.basicConfig(filename=plugin.path_to_logs(), level=logging.INFO)
-	@app.route("/"+ plugin.api_key + "/deploy", methods=["POST"])
+    logging.basicConfig(filename=get_values("path_to_logs"), level=logging.INFO)
+    # Flask initialization
+    @app.route("/", methods=["GET"])
+    def deploy():
+        if request.method == 'GET':
+            foo = print("Data received from Webhook is: ", request.json)
+            run_shell_command()
+            return foo 
+        else:
+            print("Request failed")
+    
+    app.run(host=get_values("ip_address"), port=get_values("port_number"))
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	main()
